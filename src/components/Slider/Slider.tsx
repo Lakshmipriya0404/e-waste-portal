dded707b8139fd2b6b1d895f0a47c9558c56eb92
Lakshmipriya0404 <priya42003@gmail.com>
2023-12-12 20:01:16 +0530

    add carousel

---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import img1 from "../../assets/slide1.jpg";
+import img2 from "../../assets/slide2.jpg";
+import img3 from "../../assets/slide3.jpg";
+
+const Carousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div>
+      <Slider {...settings}>
+        <div>
+          <img src={img1} alt="Image 1" />
+        </div>
+        <div>
+          <img src={img2} alt="Image 2" />
+        </div>
+        <div>
+          <img src={img3} alt="Image 3" />
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default Carousel;
--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styles from "./Home.module.css";
+import Slider from "react-slick";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={styles.homewrap}>
+      <div className={styles.first}>
+        <div className={styles.title}>E-Swachh</div>
+        <div className={styles.subtitle}>
+          The green future of e-waste disposal
+        </div>
+      </div>
+      <Slider />
+    </div>
+  );
 };
 
 export default Home;
