1b301877f9066c3b44b3eb554cce670f6390c017
Lakshmipriya0404 <priya42003@gmail.com>
2023-12-13 22:38:17 +0530

    add contact section

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import styles from "./Home.module.css";
+import { faEnvelope, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import Carousel from "../../components/Slider/Carousel";
+import artimg from "../../assets/LeafButterfly.png";
+import contactbulb from "../../assets/ContactUs_Bulb.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Home = () => {
   return (
@@ -14,6 +19,31 @@ const Home = () => {
         </div>
         <div className={styles.second}>
           <Carousel />
+        </div>
+      </div>
+      <div className={styles.container2}>
+        <div className={styles.art}>
+          <img src={artimg} alt="" />
+        </div>
+        <div className={styles.contact}>
+          <div className={styles.header}>
+            <div className="text">Contact us</div>
+            <div className="bulb">
+              <img src={contactbulb} alt="" />
+            </div>
+          </div>
+          <a className="phone" href="">
+            <FontAwesomeIcon icon={faPhone} />
+            <div className={styles.text}>+91 9876543210</div>
+          </a>
+          <a className="mail" href="">
+            <FontAwesomeIcon icon={faEnvelope} />
+            <div className={styles.text}>greenprint@gmail.com</div>
+          </a>
+          <a className="insta" href="">
+            <FontAwesomeIcon icon={faInstagram} />
+            <div className={styles.text}>The Green Print</div>
+          </a>
         </div>
       </div>
     </div>
