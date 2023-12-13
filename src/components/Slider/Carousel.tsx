a20da69d2cf79a3bfb4d9e3eb7ef94ce4c9df639
Lakshmipriya0404 <priya42003@gmail.com>
2023-12-13 18:46:34 +0530

    working carousel

--- src/components/Slider/Carousel.tsx
+++ src/components/Slider/Carousel.tsx
@@ -1,16 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./Carousel.module.css";
 import img1 from "../../assets/slide1.jpg";
 import img2 from "../../assets/slide2.jpeg";
 import img3 from "../../assets/slide3.jpeg";
 
-const Carousel: React.FC = () => {
+interface CarouselProps {}
+
+const Carousel: React.FC<CarouselProps> = () => {
   const [activeIndex, setActiveIndex] = useState(0);
 
   const handleButtonClick = (offset: number) => {
     const newIndex = (activeIndex + offset + 3) % 3; // Assuming 3 slides in the carousel
     setActiveIndex(newIndex);
   };
+
+  useEffect(() => {
+    const handleClick = (event: Event) => {
+      const button = event.target as HTMLElement;
+      const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+
+      const slides = document.querySelector(
+        "[data-slides]"
+      ) as HTMLElement | null;
+      if (!slides) return;
+
+      const activeSlide = slides.querySelector(
+        "[data-active]"
+      ) as HTMLElement | null;
+      if (!activeSlide) return;
+
+      const slidesArray = Array.from(slides.children);
+      const currentIndex = slidesArray.indexOf(activeSlide);
+      const newIndex =
+        (currentIndex + offset + slidesArray.length) % slidesArray.length;
+
+      slidesArray.forEach((slide, index) => {
+        if (index === newIndex) {
+          slide.setAttribute("data-active", "true");
+        } else {
+          slide.removeAttribute("data-active");
+        }
+      });
+    };
+
+    const buttons = document.querySelectorAll("[data-carousel-button]");
+    buttons.forEach((button) => {
+      button.addEventListener("click", handleClick);
+    });
+
+    return () => {
+      buttons.forEach((button) => {
+        button.removeEventListener("click", handleClick);
+      });
+    };
+  }, [activeIndex]);
 
   return (
     <div className={styles.carousel}>
