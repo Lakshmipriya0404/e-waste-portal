de9e72b8cecb2d17fcb24b12abd77c03f8fbfe91
Lakshmipriya0404 <priya42003@gmail.com>
2023-12-15 23:50:22 +0530

    add main dashboard

--- src/routes/Dashboard/Dashboard.tsx
+++ src/routes/Dashboard/Dashboard.tsx
@@ -1,51 +1,17 @@
 import React from "react";
 import styles from "./Dashboard.module.css";
-import {
-  faUser,
-  faMapMarkerAlt,
-  faCalendar,
-  faClock,
-  faAngleRight,
-} from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import profile from "../../assets/profile.png";
+import Sidebar from "../../sections/Sidebar/Sidebar";
+import Profile from "../../sections/Profile/Profile";
+import FacilityLocator from "../../sections/Locator/FacilityLocator";
 
 const Dashboard = () => {
-  const itemsListIcon = [
-    faUser,
-    faMapMarkerAlt,
-    faCalendar,
-    faClock,
-    faAngleRight,
-  ];
-  const itemsListName = [
-    "Profile",
-    "Nearest E-waste Facilitator",
-    "Recent Requests",
-    "Disposal History",
-    "Signed Out",
-  ];
-
   return (
     <div className={styles.mainwrap}>
-      <div className={styles.sideboard}>
-        <img src={profile} alt="" />
-        <div className={styles.welcome}>Hello User</div>
-        <button className={styles.sidebtn}>Dashboard</button>
-        <div className={styles.sidebarlist}>
-          <ul>
-            {itemsListIcon.map((icon, index) => (
-              <li key={index}>
-                <div className={styles.item}>
-                  <FontAwesomeIcon icon={icon} />
-                  <div className={styles.text}>{itemsListName[index]}</div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
+      <Sidebar />
+      <div className={styles.mainboard}>
+        <Profile />
+        <FacilityLocator />
       </div>
-      <div className={styles.mainboard}></div>
     </div>
   );
 };
---
+++ src/sections/Sidebar/Sidebar.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+  faUser,
+  faMapMarkerAlt,
+  faCalendar,
+  faClock,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import profile from "../../assets/profile.png";
+import styles from "./Sidebar.module.css";
+
+const Sidebar = () => {
+  const itemsListIcon = [
+    faUser,
+    faMapMarkerAlt,
+    faCalendar,
+    faClock,
+    faAngleRight,
+  ];
+  const itemsListName = [
+    "Profile",
+    "Nearest E-waste Facilitator",
+    "Recent Requests",
+    "Disposal History",
+    "Signed Out",
+  ];
+  return (
+    <div className={styles.sideboard}>
+      <img src={profile} alt="" />
+      <div className={styles.welcome}>Hello User</div>
+      <button className={styles.sidebtn}>Dashboard</button>
+      <div className={styles.sidebarlist}>
+        <ul>
+          {itemsListIcon.map((icon, index) => (
+            <li key={index}>
+              <div className={styles.item}>
+                <FontAwesomeIcon icon={icon} />
+                <div className={styles.text}>{itemsListName[index]}</div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
