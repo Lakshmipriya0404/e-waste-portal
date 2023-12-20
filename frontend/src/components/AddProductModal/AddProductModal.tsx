b95005db4a0349f88284f955a2930b920dba341a
Deepika-Govindaswamy <deepikagovindaswamy@gmail.com>
2023-12-20 12:17:45 +0530

    addproduct

--- frontend/src/components/AddProductModal/AddProductModal.tsx
+++ frontend/src/components/AddProductModal/AddProductModal.tsx
@@ -45,66 +45,74 @@ const AddProductModal = ({ onClose, onAddProduct }) => {
   return (
     <div className={styles.popupcontainer}>
       <div className={styles.popupbody}>
+        <h1>Add Product</h1>
         <div className={styles.popupcontents}>
-          <div className={styles.modelname}>
-            <label>Model Name</label>
-            <input
-              type="text"
-              id="name"
-              className={styles.input_text}
-              name="name"
-              placeholder="Name"
-            />
-          </div>
-          <div className={styles.category}>
-            <label htmlFor="category">Category</label>
+          <form action="">
+            <div className={styles.modelname}>
+              <label>Model Name</label>
+              <input
+                type="text"
+                id="name"
+                className={styles.input_text}
+                name="name"
+                placeholder="Name"
+              />
+            </div>
 
-            <select name="category" id="category">
-              <option value="processor">Centralized Data Processing</option>
-              <option value="PC">PC</option>
-              <option value="Laptop">Laptop</option>
-              <option value="Printer">Printer</option>
-              <option value="Typewriters">Typewriters</option>
-              <option value="Settopbox">Settop Box</option>
-              <option value="Smartphones">Smartphones</option>
-              <option value="TV">TV</option>
-              <option value="Refridgerator">Refridgerator</option>
-              <option value="AC">AC</option>
-              <option value="UPS">UPS</option>
-              <option value="Routers">Routers</option>
-              <option value="Modems">Modems</option>
-              <option value="Flashdrives">Flash Drives</option>
-              <option value="Hairdryers">Hairdryers</option>
-            </select>
-          </div>
+            <div className={styles.category}>
+              <label htmlFor="category">Category</label>
 
-          <div className={styles.condition}>
-            <label htmlFor="condition">Condition</label>
-            <select name="condition" id="condition">
-              <option value="working">Working</option>
-              <option value="notworking">Not Working</option>
-              <option value="damaged">Damaged</option>
-            </select>
-          </div>
-
-          <div className={styles.images}>
-            <label htmlFor="imageUpload">Choose an image:</label>
-            <input
-              type="file"
-              id="imageUpload"
-              name="image"
-              accept="image/*"
-              onChange={handleFileChange}
-            />
-          </div>
-        </div>
-        <div>
-          <button className={styles.addbutton} onSubmit={handleAddProductClick}>
-            Add Product
-          </button>
-          <button className={styles.addbutton} onClick={handleCloseButtonClick}>
-            Close
-          </button>
+              <select name="category" id="category">
+                <option value="processor">Centralized Data Processing</option>
+                <option value="PC">PC</option>
+                <option value="Laptop">Laptop</option>
+                <option value="Printer">Printer</option>
+                <option value="Typewriters">Typewriters</option>
+                <option value="Settopbox">Settop Box</option>
+                <option value="Smartphones">Smartphones</option>
+                <option value="TV">TV</option>
+                <option value="Refridgerator">Refridgerator</option>
+                <option value="AC">AC</option>
+                <option value="UPS">UPS</option>
+                <option value="Routers">Routers</option>
+                <option value="Modems">Modems</option>
+                <option value="Flashdrives">Flash Drives</option>
+                <option value="Hairdryers">Hairdryers</option>
+              </select>
+            </div>
+            <div className={styles.condition}>
+              <label htmlFor="condition">Condition</label>
+              <select name="condition" id="condition">
+                <option value="working">Working</option>
+                <option value="notworking">Not Working</option>
+                <option value="damaged">Damaged</option>
+              </select>
+            </div>
+            <div className={styles.images}>
+              <label htmlFor="imageUpload">Choose an image:</label>
+              <input
+                type="file"
+                id="imageUpload"
+                name="image"
+                accept="image/*"
+                onChange={handleFileChange}
+              />
+            </div>
+            <div className={styles.button}>
+              <button
+                className={styles.addbutton}
+                onSubmit={handleAddProductClick}
+              >
+                Add Product
+              </button>
+              <button
+                className={styles.addbutton}
+                onClick={handleCloseButtonClick}
+              >
+                Close
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
